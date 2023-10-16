fa2167de3404bca210d480035822fbf3b8834b38
manojkumarm1310 <132912460+manojkumarm1310@users.noreply.github.com>
2023-10-16 14:02:27 +0530

    deploy

---
+++ game.js
@@ -0,0 +1,86 @@
+var gamePattern=[]; 
+var userClickedPattern=[];
+var level=0;
+var started=false;
+var buttonColours=["green","red","purple","white","blue","orange"];
+
+$(document).keypress(function(){
+    if (!started) {
+        $("h1").text("Level " + level);
+        nextSequence();
+        started = true;
+      }
+});
+
+$("button").click(function()
+{
+    var userChosenColour=$(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playAudio(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+});
+
+
+function checkAnswer(currentlevel)
+{
+    if (gamePattern[currentlevel] === userClickedPattern[currentlevel]) {
+        if (userClickedPattern.length === gamePattern.length){
+          setTimeout(function () {
+            nextSequence();
+          }, 1000);
+        }
+      } else {
+        playAudio("wrong");
+        $("body").addClass("game-over");
+        $("h1").text("Game Over, Press Any Key to Restart");
+  
+        setTimeout(function () {
+          $("body").removeClass("game-over");
+        }, 200);
+  
+        startOver();
+      }
+}
+
+function nextSequence()
+{
+    userClickedPattern=[];
+    level++;
+    $("h1").text("Level "+ level);
+    var randomNumber=Math.floor((Math.random())*6);
+    var randomChosencolour = buttonColours[randomNumber];
+    gamePattern.push(randomChosencolour);
+    $("#"+ randomChosencolour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playAudio(randomChosencolour);
+   
+   
+}
+
+
+function playAudio(randomChosencolour)
+{
+    var audio=new Audio("sounds/"+randomChosencolour+".mp3");
+    audio.play();
+}
+
+
+
+function animatePress(currentColour)
+{
+ $("#" + currentColour).addClass("pressed");   
+ setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+
+
+
+
+function startOver()
+{
+    level = 0;
+  gamePattern = [];
+  started = false;
+}
